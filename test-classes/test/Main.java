861f038288a6d09423f324896ed9d8c8b68ccf54
Shashin Halalingaiah <shashin.halalingaiah@gmail.com>
2022-06-10 10:47:30 +0530

    backup

--- test-classes/test/Main.java
+++ test-classes/test/Main.java
@@ -1,14 +1,39 @@
 class Main {
 	public static void main (String [] args) {
-		A a = new A();
-		a.f = new F();;
-		a.g = new G();
-		a.h = new H();
+		A a1 = new A();
+		A a2 = new A();
+		A a3 = new A();
+
+		a1.f = new F();
+		a2.f = new F();
+		a3.f = new F();
+
+		boolean x = false;
+		int y = 100;
+		while(x) {
+//			int z;
+//			if(x) 
+//				z = 99;
+//			else z = 109;
+			a3.f = a2.f;
+			a2.f = a1.f;
+
+			//x = !x;
+			//y--;
+		}
+
+		F f1 = a3.f;
+		F f2 = a2.f;
+		F f3 = a1.f;
 	}
 }
 
-class F {}
-class G {}
-class H {}
+class A {
+	F f;
+}
 
-class A { F f; G g; H h; }
+class F { }
+
+class B {
+	boolean getBool() { return false; }
+}
